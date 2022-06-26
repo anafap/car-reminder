0655ff0380e2c342a0daadc2600dd0ddfd2e9205
Anafa <anafap10@gmail.com>
2022-06-27 01:40:48 +0530

    Integrated a local database (Nedb) and updated the existing APIs

--- index.js
+++ index.js
@@ -1,49 +1,77 @@
 const express = require("express");
 var bodyParser = require("body-parser");
+var Datastore = require("nedb");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-const cars = [
-    {
-        id: 1,
-        name: "Polo GT",
-        mileage: 65000,
-    },
-    {
-        id: 2,
-        name: "Skoda Yeti",
-        mileage: 50000,
-    },
-];
+const carsDb = new Datastore("database.db");
+carsDb.loadDatabase();
+carsDb.find({}, function (err, docs) {
+    if (docs.length === 0) {
+        const cars = [
+            {
+                id: 1,
+                name: "Polo GT",
+                mileage: 65000,
+            },
+            {
+                id: 2,
+                name: "Skoda Yeti",
+                mileage: 50000,
+            },
+        ];
+        carsDb.insert(cars, function (err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(`Inserted sample data of ${cars.length} cars`);
+            }
+        });
+    }
+});
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.get("/cars", function (req, res) {
-    res.json(cars);
+    carsDb.find({}, { _id: 0 }, function (err, cars) {
+        if (err) {
+            return res.status(500).json({
+                result: "fail",
+                message: "Database error",
+            });
+        }
+        console.log("get cars success");
+        res.json(cars);
+    });
 });
 
 app.get("/cars/:id", function (req, res) {
-    const id = req.params.id;
-    var searchIndex = -1;
-    cars.forEach((element, index) => {
-        if (element.id === parseInt(id)) {
-            searchIndex = index;
+    const carId = parseInt(req.params.id);
+    carsDb.find({ id: carId }, { _id: 0 }, function (err, cars) {
+        if (err) {
+            return res.status(500).json({
+                result: "fail",
+                message: "Database error",
+            });
         }
+
+        if (cars.length === 0) {
+            return res.status(404).json({
+                result: "fail",
+                message: "No such car",
+            });
+        }
+        res.json(cars[0]);
     });
-    if (searchIndex != -1) {
-        res.json(cars[searchIndex]);
-    } else {
-        res.json({ result: "fail" });
-    }
 });
 
 app.post("/cars", function (req, res) {
-    const maxId = Math.max(
+    /*  const maxId = Math.max(
         ...cars.map(function (element) {
             return element.id;
         })
@@ -54,6 +82,39 @@ app.post("/cars", function (req, res) {
         name: req.body.name,
         mileage: req.body.mileage,
     });
+    carsDb.insert(cars);
+    res.json({ result: "success" }); */
+
+    carsDb
+        .find({})
+        .sort({ id: -1 })
+        .limit(1)
+        .projection({ id: 1, _id: 0 })
+        .exec(function (err, cars) {
+            console.log("cars", cars[0]);
+            const maxId = cars[0].id;
+            console.log("maxid ", maxId);
+            const newCar = {
+                id: maxId + 1,
+                name: req.body.name,
+                mileage: req.body.mileage,
+            };
+
+            carsDb.insert(newCar, function (err, cars) {
+                console.log("insertion success");
+            });
+        });
+
+    /*
+    console.log("carId", carsId);
+    var nextId = carsId + 1;
+    console.log(nextId);
+    carsDb.count({}, function (err, count) {
+        console.log("count",count);
+        newCar = {
+            name: req.body.name,
+        } */
+
     res.json({ result: "success" });
 });
 
